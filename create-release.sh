1ec24d464ed1c91655551c94d2fddd415b189b12
Ruby <55691511+okpc579@users.noreply.github.com>
2024-01-08 11:20:52 +0900

    create-release rename
    Signed-off-by: Ruby <55691511+okpc579@users.noreply.github.com>

--- create-release.sh
+++ create-release.sh
@@ -1,8 +1,8 @@
 echo \
 '/****************************************************************
-*	${1} : release-name	                                *
-*	${2} : version						*	
-*       # ./create-release  paasta-rabbitmq 2.1.0               *
+*	${1} : release-name
+*	${2} : version
+* # ./create-release  ap-rabbitmq 2.1.0
 ****************************************************************/
 '
 bosh create-release --name ${1} --sha2 --force --tarball ./${1}-release-${2}.tgz --version ${2}
